4b032714aeec69b3595e1b7f2e3d376a843a1e6d
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2024-10-19 22:05:58 +0600

    Update file and add index.html

--- BMI-calculator.js
+++ BMI-calculator.js
@@ -4,14 +4,10 @@ let height = prompt("Enter your height in m: ")
 let BMI = weight / (height * height)
 if (BMI < 18.5) {
    console.log("Your BMI is " + BMI + " and you are underweight.")
-}
-
-else if (BMI >= 18.5 && BMI <= 24.9) {
+} else if (BMI >= 18.5 && BMI <= 24.9) {
    console.log("Your BMI is " + BMI + " and you are normal.")
-}
-else if (BMI >= 25 && BMI <= 29.9) {
+} else if (BMI >= 25 && BMI <= 29.9) {
    console.log("Your BMI is " + BMI + " and you are overweight.")
-}
-else {
+} else {
    console.log("Your BMI is " + BMI + " and you are obese.")
 }
